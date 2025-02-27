ec77b9cf93cf814148b8510581c9fa1f496cc0bb
Luckamarques720 <lukamarques.51@gmail.com>
2025-02-27 01:37:24 +0000

    Código Atualizado.2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,13 +5,23 @@ int main() {
    char Estado;
    char Cod_carta;
    char cidade;
-   int população;
+   int populacao;
    float Area;
    float PIB;
    int Pontos_turisticos;
-
+  
+   char Estado2;
+   char Cod_carta2;
+   char cidade2;
+   int populacao2;
+   float Area2;
+   float PIB2;
+   int Pontos_turisticos2;
+ 
+   /*Dados da primeira carta*/
+  
    /*Pede a letra que representará um dos Estados*/
-   pintf("Letra: ");
+   printf("Letra: ");
    scanf("%s", &Estado);
 
    /*Pede o código da carta*/
@@ -24,11 +34,11 @@ int main() {
 
    /*Pede a população*/
    printf("População: ");
-   scanf("%d", &população);
+   scanf("%d", &populacao);
 
    /*Pede a Área*/
    printf("Área: ");
-   sacanf("%f", &Area);
+   scanf("%f", &Area);
 
    /*Pede o PIB*/
    printf("PIB: ");
@@ -38,6 +48,37 @@ int main() {
    printf("Número de pontos turísticos: ");
    scanf("%d", &Pontos_turisticos);
 
+   /*Dados da segunda carta*/
+
+    /*Pede a letra que representará um dos Estados*/
+    printf("Letra: ");
+    scanf("%s", &Estado2);
+ 
+    /*Pede o código da carta*/
+    printf("Código da Carta: ");
+    scanf("%s", &Cod_carta2);
+ 
+    /*Pede a população*/
+    printf("Nome da Cidade: ");
+    scanf("%s", &cidade2);
+ 
+    /*Pede a população*/
+    printf("População: ");
+    scanf("%d", &populacao2);
+ 
+    /*Pede a Área*/
+    printf("Área: ");
+    scanf("%f", &Area2;
+ 
+    /*Pede o PIB*/
+    printf("PIB: ");
+    scanf("%f", &PIB2);
+ 
+    /*Pede o número de pontos turísticos*/
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &Pontos_turisticos2);
+
+    /*Mostrando as informações na tela*/
 
     return 0;
 }
---
+++ Desafio_Super_Trunfo.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+  /*Declaração das variáveis*/
+   char Estado;
+   char Cod_carta;
+   char cidade;
+   int população;
+   float Area;
+   float PIB;
+   int Pontos_turisticos;
+
+   /*Pede a letra que representará um dos Estados*/
+   pintf("Letra: ");
+   scanf("%s", &Estado);
+
+   /*Pede o código da carta*/
+   printf("Código da Carta: ");
+   scanf("%s", &Cod_carta);
+
+   /*Pede a população*/
+   printf("Nome da Cidade: ");
+   scanf("%s", &cidade);
+
+   /*Pede a população*/
+   printf("População: ");
+   scanf("%d", &população);
+
+   /*Pede a Área*/
+   printf("Área: ");
+   sacanf("%f", &Area);
+
+   /*Pede o PIB*/
+   printf("PIB: ");
+   scanf("%f", &PIB);
+
+   /*Pede o número de pontos turísticos*/
+   printf("Número de pontos turísticos: ");
+   scanf("%d", &Pontos_turisticos);
+
+
+
+    return 0;
+}
